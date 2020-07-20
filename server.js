4e40779cf77fd78ebacf6a0e01f0cf29c66fe17e
kcapasso-mcdaniel <64052620+kcapasso-mcdaniel@users.noreply.github.com>
2020-07-19 20:09:36 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const mysql = require("mysql");
+// function call created connection that takes an object that is all of our database information
+const connection = mysql.createConnection({
+  host: process.env.RDS_HOST,
+  user: process.env.RDS_USER,
+  password: process.env.RDS_PASSWORD,
+  database: "white_bear_app",
+});
+
+connection.connect();
+
+// can run a query
+connection.query(
+  `
+   SELECT 
+      users.id 
+   FROM 
+      users 
+   WHERE 
+      users.email = 'kate@gmail.com' 
+   AND 
+      users.password = 'replace_me'`,
+  (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(results);
+    }
+  }
+);
+
+connection.end();
