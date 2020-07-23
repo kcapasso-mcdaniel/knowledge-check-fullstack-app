d2b31063e6b8f1d7d0ce3bf026bc49ac85852afb
kcapasso-mcdaniel <64052620+kcapasso-mcdaniel@users.noreply.github.com>
2020-07-23 14:01:51 -0700

    add heroku build to scripts

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 // express
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use("/api/v1/users", require("./api/v1/users"));
 app.use("/api/v1/user-questions", require("./api/v1/user-questions"));
 app.use("/api/v1/assigned-questions", require("./api/v1/assigned-questions"));
 app.use("/api/v1/all-users", require("./api/v1/all-users"));
-app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 // keep port as an environment variable
 const port = process.env.PORT || 5001;
