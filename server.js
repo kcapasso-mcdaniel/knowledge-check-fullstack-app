ad90b978a06f70b7fd3522ceb41c898a8ae694ef
kcapasso-mcdaniel <64052620+kcapasso-mcdaniel@users.noreply.github.com>
2020-07-23 14:35:22 -0700

    fix scripts syntax

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use("/api/v1/user-questions", require("./api/v1/user-questions"));
 app.use("/api/v1/assigned-questions", require("./api/v1/assigned-questions"));
 app.use("/api/v1/all-users", require("./api/v1/all-users"));
 
+// if not route is built use build folder
 app.use(express.static("client/build"));
 app.get("*", (req, res) => {
    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
